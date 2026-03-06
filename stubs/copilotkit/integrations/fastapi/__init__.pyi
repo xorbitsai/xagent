138e5981450fd787a131ee6f0b25782072ad15df
qinxuye <qinxuye@gmail.com>
2026-03-06 21:10:35 +0800

    chore(deps): remove unused copilotkit dependency

--- stubs/copilotkit/integrations/fastapi/__init__.pyi
+++
@@ -1,7 +0,0 @@
-from typing import Any
-
-from ... import CopilotKitRemoteEndpoint
-
-def add_fastapi_endpoint(
-    app: Any, sdk: CopilotKitRemoteEndpoint, prefix: str = "/copilotkit"
-) -> None: ...

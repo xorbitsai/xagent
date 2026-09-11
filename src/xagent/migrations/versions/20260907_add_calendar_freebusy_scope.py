2f2db033d202d0e469f98ffadf5c7d6ced02a325
yiboyasss <3359595624@qq.com>
2026-09-11 16:04:24 +0800

    fix(calendar): follow latest migration head and normalize attendees

--- src/xagent/migrations/versions/20260907_add_calendar_freebusy_scope.py
+++ src/xagent/migrations/versions/20260907_add_calendar_freebusy_scope.py
@@ -34,7 +34,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision: str = "20260907_add_calendar_freebusy_scope"
-down_revision: Union[str, None] = "20260909_seed_shopify_mcp_app"
+down_revision: Union[str, None] = "20260911_assistant_source_event"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 

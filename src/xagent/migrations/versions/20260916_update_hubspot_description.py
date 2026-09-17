bd8a68a59c1c2106efa79017a905e794df1d8402
yiboyasss <3359595624@qq.com>
2026-09-17 18:27:31 +0800

    fix(hubspot): rebase migration onto the new alembic head after merging main again
    The merge queue rejected this PR: main had merged PR #2449 (Deputy
    create/update tools), which added 20260916_update_deputy_description
    chaining from the same 20260916_merge_delivery_memory revision this
    migration's down_revision pointed to - producing two heads again, the
    same class of conflict fixed once already in this PR. Merge main and
    point down_revision at the new true head.

--- src/xagent/migrations/versions/20260916_update_hubspot_description.py
+++ src/xagent/migrations/versions/20260916_update_hubspot_description.py
@@ -1,7 +1,7 @@
 """update HubSpot connector description for the new list/search tools
 
 Revision ID: 20260916_update_hubspot_description
-Revises: 20260916_merge_delivery_memory
+Revises: 20260916_update_deputy_description
 Create Date: 2026-09-16
 
 """
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision: str = "20260916_update_hubspot_description"
-down_revision: Union[str, None] = "20260916_merge_delivery_memory"
+down_revision: Union[str, None] = "20260916_update_deputy_description"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 

1ead8c5b220f202157193a73cfb04182aaf65473
yiboyasss <3359595624@qq.com>
2026-09-17 13:35:45 +0800

    fix(hubspot): rebase migration onto the new alembic head after merging main
    main added 20260916_merge_delivery_memory since this branch forked, which
    also chains from 20260911_global_memory_authority - the same revision this
    migration's down_revision pointed to. That left two alembic heads once
    merged, failing CI's alembic-check pre-commit hook and the SQLite/PostgreSQL
    migration test jobs. Point down_revision at the new true head instead.

--- src/xagent/migrations/versions/20260916_update_hubspot_description.py
+++ src/xagent/migrations/versions/20260916_update_hubspot_description.py
@@ -1,7 +1,7 @@
 """update HubSpot connector description for the new list/search tools
 
 Revision ID: 20260916_update_hubspot_description
-Revises: 20260911_global_memory_authority
+Revises: 20260916_merge_delivery_memory
 Create Date: 2026-09-16
 
 """
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision: str = "20260916_update_hubspot_description"
-down_revision: Union[str, None] = "20260911_global_memory_authority"
+down_revision: Union[str, None] = "20260916_merge_delivery_memory"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 

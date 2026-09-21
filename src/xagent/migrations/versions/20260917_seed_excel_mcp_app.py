9cb50ec7a533d82020054b212254e78f55f3492e
yiboyasss <3359595624@qq.com>
2026-09-21 12:06:44 +0800

    fix(migrations): rebase Excel seed onto latest head

--- src/xagent/migrations/versions/20260917_seed_excel_mcp_app.py
+++ src/xagent/migrations/versions/20260917_seed_excel_mcp_app.py
@@ -1,7 +1,7 @@
 """seed built-in Excel (OAuth) MCP connector
 
 Revision ID: 20260917_seed_excel_mcp_app
-Revises: 91899e1d97d3
+Revises: 6f40e43d9e28
 Create Date: 2026-09-17 00:00:00.000000
 
 """
@@ -21,7 +21,7 @@ logger = logging.getLogger(__name__)
 
 # revision identifiers, used by Alembic.
 revision: str = "20260917_seed_excel_mcp_app"
-down_revision: Union[str, None] = "91899e1d97d3"
+down_revision: Union[str, None] = "6f40e43d9e28"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
